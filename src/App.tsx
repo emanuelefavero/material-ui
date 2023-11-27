ee987cff4ea68e76e5ce91540976094de50ff069
emanuelefavero <emafaux89@gmail.com>
2023-11-27 18:56:48 +0100

    setup first material ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Button from '@mui/material/Button'
 
 export default function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Button variant='contained'>Ciao</Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Material UI CSS reset
+// @see https://mui.com/material-ui/react-css-baseline
+import CssBaseline from '@mui/material/CssBaseline'
+
 // Material UI fonts
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -11,6 +15,7 @@ import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
