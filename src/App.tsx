47a4eed106b4deec92c95db487d6c05587f01e55
emanuelefavero <emafaux89@gmail.com>
2024-03-10 19:54:57 +0100

    add tailwind and autocomplete options

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,32 @@ import TextField from '@mui/material/TextField'
 
 export default function App() {
   return (
-    <>
-      <h1>Material UI</h1>
+    <main className='max-w-screen-sm'>
+      <h1 className='text-3xl font-bold mb-8'>Material UI</h1>
 
-      {/* BUTTON */}
-      <Button variant='contained'>Button</Button>
+      <ul>
+        <li>
+          {/* BUTTON */}
+          <Button variant='contained'>Button</Button>
+        </li>
 
-      {/* AUTOCOMPLETE */}
-      <Autocomplete
-        options={['Titanic', 'Option Jurassic Park']}
-        renderInput={(params) => <TextField {...params} label='Movie' />}
-      />
-    </>
+        <li>
+          {/* AUTOCOMPLETE */}
+          <Autocomplete
+            options={autocompleteOptions}
+            renderInput={(params) => <TextField {...params} label='Movie' />}
+          />
+        </li>
+      </ul>
+    </main>
   )
 }
+
+// Utils and Data
+const autocompleteOptions = [
+  { label: 'Titanic', year: 1997 },
+  { label: 'Jurassic Park', year: 1993 },
+  { label: 'The Lion King', year: 1994 },
+  { label: 'The Matrix', year: 1999 },
+  { label: 'The Lord of the Rings', year: 2001 },
+]
