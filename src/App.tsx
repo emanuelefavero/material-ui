38a8bc8e865eac18dfa526bc193e253279edefe2
emanuelefavero <emafaux89@gmail.com>
2024-03-10 19:13:12 +0100

    add autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css'
 import Button from '@mui/material/Button'
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
 
 export default function App() {
   return (
     <>
       <h1>Material UI</h1>
 
-      <br />
-
+      {/* BUTTON */}
       <Button variant='contained'>Button</Button>
+
+      {/* AUTOCOMPLETE */}
+      <Autocomplete
+        options={['Titanic', 'Option Jurassic Park']}
+        renderInput={(params) => <TextField {...params} label='Movie' />}
+      />
     </>
   )
 }
